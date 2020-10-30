23435c2e2bfdce45fed498da907d61935a8b1dde
YZ.Chew <YZ.Chew@doo.hk>
2020-10-30 15:20:51 +0800

    Add apollo server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,42 @@
 const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const users = {
+    1: {
+        id: 1,
+        username: 'John Doe'
+    }
+}
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        id: ID!
+        username: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        me: () => {
+            return users[1]
+        }
+    }
+}
 
 const app = express()
 
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+// Start the server
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
